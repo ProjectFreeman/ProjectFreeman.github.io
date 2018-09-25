64b8201a48fd225ad086bb9f669576a11ee716ef
wallmenis <sandboxgamergr@hotmail.com>
2018-09-25 22:12:21 +0300

    updates and shit to work on most browsers... i still struggle with the "go to top" button...

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,12 @@
 ﻿window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("btn").style.display = "block";
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        if (document.getElementById("btn").style.display = "none")
+        {
+            document.getElementById("btn").style.display = "block";
+            document.getElementById("btn").style.zIndex = 99;
+        }
     } else {
         document.getElementById("btn").style.display = "none";
     }
@@ -12,28 +16,33 @@ function go_to_about() {
     var alignToTop = true
     var about = document.getElementById("about");
     about.scrollIntoView(alignToTop);
+    document.getElementById("btn").style.display = "block";
 }
 
 function go_to_pf() {
     var alignToTop = true
     var pf = document.getElementById("pf");
     pf.scrollIntoView(alignToTop);
+    document.getElementById("btn").style.display = "block";
 }
 
 function go_to_cu() {
     var alignToTop = true
     var cu = document.getElementById("cu");
     cu.scrollIntoView(alignToTop);
+    document.getElementById("btn").style.display = "block";
 } 
 
 function go_to_tt() {
     var alignToTop = true
     var tt = document.getElementById("tt");
     tt.scrollIntoView(alignToTop);
+    document.getElementById("btn").style.display = "block";
 }
 
 function go_to_top() {
-    var alignToTop = true
-    var top = document.getElementById("top");
-    top.scrollIntoView(alignToTop);
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+    document.getElementById("btn").style.display = "none";
 }
